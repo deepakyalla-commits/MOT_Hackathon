d322185df3e1828167cf5e7de68203f80db318b9
deepakyalla-commits <81838569+deepakyalla-commits@users.noreply.github.com>
2021-04-03 13:59:21 +0530

    Create Spark_processing.sh

---
+++ Spark_processing.sh
@@ -0,0 +1,64 @@
+ val spark = SparkSession.builder.appName("proj - module").getOrCreate()  
+    # Create a Spark Context
+    val sc = spark.sparkContext
+
+ 
+
+    # Set Log level in Spark
+    sc.setLogLevel("WARN")
+    
+    //cust key,cust name, cust res country
+    val custinfoDF=spark.read.format("csv").schema(csvSchema).load("/user/user4/user/customer_info.csv")
+    
+    //account key, cust key, account open date
+    val custaccountInfoDF=spark.read.format("csv").option("header",true).load("/user/user2/user/customer_account_info.csv")
+
+ 
+
+    // transfer_key, account_key,transaction_amt,transaction_type,country_code,transfer_due_date
+    val transDF=spark.read.format("csv").option("header",true).load("/user/user4/user/customer_transaction.csv")
+
+ 
+
+    val countriesDf = spark.read.format("csv").option("header",true).option("header","true").load("/user/user4/user/countries_info.csv")              
+
+ 
+
+
+    //cust name, cust res country, account key, cust_key, account open date
+    custinfoAccounDf =  custinfoDF.join(custaccountInfoDF,custinfoDF.customer_key == custaccountInfoDF.customer_key,"inner") \
+                            .drop(custaccountInfoDF.customer_key)
+
+ 
+
+    val custinfoAccounDf =  custinfoDF.join(custaccountInfoDF,custinfoDF.party_key === custaccountInfoDF.primary_party_key,"inner")    
+    val custinfoAccounDf =  custinfoDF.join(custaccountInfoDF,custinfoDF("party_key") == custaccountInfoDF("primary_party_key"),"inner")    
+
+ 
+
+
+    val finalDf = custinfoAccounDf.join(transDF,custinfoAccounDf.account_key == transDF.account_key,"inner")
+                            .drop(transDF.account_key)                         
+ 
+    val finalwithCountriesDf = finalDf.join(countriesDf,finalDf.country_code == countriesDf.entity_key,"left")
+    
+
+ 
+
+    finalwithCountriesDf.cache()
+    
+    val HRDF = finalwithCountriesDf.filter(entity_key is not null || \
+    (sum(transaction_amt) > 1000 && transaction_type = 'IN') || (sum(transaction_amt) > 800 && transaction_type = 'OUT') ")\
+    .withColumn("Risk_level",lit("HR"))
+                         
+    val MedDF = finalDf.filter("(customer_residential_country == 'PK' || customer_residential_country == 'AFG') || \
+    (sum(transaction_amt) > 1000 && transaction_type = 'IN') || (sum(transaction_amt) > 800 && transaction_type = 'OUT') ")\
+    .withColumn("Risk_level",lit("LR"))    
+
+ 
+
+
+    val lowDF = finalDf.filter("(customer_residential_country == 'PK' || customer_residential_country == 'AFG') || \
+    (sum(transaction_amt) > 1000 && transaction_type = 'IN') || (sum(transaction_amt) > 800 && transaction_type = 'OUT') ")
+    .withColumn("Risk_level",lit("LR"))
+    
